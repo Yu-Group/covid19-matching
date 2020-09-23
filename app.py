ddb5ebe517d78d80a2a837d8ceb64e8646015ac3
Chandan Singh <csinva@yahoo.com>
2020-09-23 04:52:48 +0000

    minor app improvements

--- app.py
+++ app.py
@@ -11,9 +11,11 @@ import pickle
 from datetime import datetime, timedelta
 import os
 from os.path import join as oj
-from config import data_dir, viz_dir
+from config import data_dir, viz_dir, assets_dir
 import nn
-
+import sys
+sys.path.append(viz_dir)
+import viz_map_utils
 
 
 
@@ -156,15 +158,16 @@ def redefine_app(app, df):
 
     )
 
-
+    
+def add_callbacks(app, df):
     # Callback for timeseries price
     @app.callback(Output('timeseries', 'figure'),
                   [Input('countyselector', 'value'),
                    Input('nnfeatselector', 'value')])
     def update_graph(selected_counties, selected_features):
-        print(selected_counties)
-        print(selected_features)
-        print(isinstance(selected_counties, list))
+        print('selected_counties', selected_counties, 'selected_features', selected_features)
+        # print(selected_features)
+        # print(isinstance(selected_counties, list))
         cols = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', 
             '#f58231', '#911eb4', '#46f0f0', '#f032e6', 
             '#bcf60c', '#fabebe', '#008080', '#e6beff', 
@@ -174,9 +177,6 @@ def redefine_app(app, df):
         fig = make_subplots(rows=2, cols=2,
                             subplot_titles=("Cases", "Deaths", "Cases (Aligned to first case)", "Deaths (Aligned to first death)"))
         keys = ['cases', 'deaths', 'aligned_cases', 'aligned_deaths']
-        import sys
-        sys.path.append(viz_dir)
-        import viz_map_utils
         date1 = viz_map_utils.date_in_data(df)
     #     print(date1)
         date2 = [i for i in range(len(date1))]
@@ -217,7 +217,7 @@ def redefine_app(app, df):
             if selected_features is not None:
                 if len(selected_features)!= 0:
                     neighs = nn.model(df, selected_features, selected_counties)
-                    print(neighs)
+                    # print(neighs)
                     for i in range(4):
                         key = keys[i]
                         for index, pos in enumerate(neighs):
@@ -226,7 +226,7 @@ def redefine_app(app, df):
                                 dates = date1
                             else:
                                 dates = date2
-                            print(row['CountyName'].values[0] +', ' + row['StateName'].values[0])
+                            # print(row['CountyName'].values[0] +', ' + row['StateName'].values[0])
                             fig.add_trace(go.Scatter(x=dates,
                                         y=row[key].to_list()[0],
                                         line=dict(color=cols[index+1]),
@@ -251,6 +251,95 @@ def redefine_app(app, df):
         fig['layout']['yaxis4']['title']='Deaths'                                
         return fig
 
+    def save_screenshot(selected_counties='Alameda, CA', selected_features=['recent_deaths']):
+        print('selected_counties', selected_counties, 'selected_features', selected_features)
+        # print(selected_features)
+        # print(isinstance(selected_counties, list))
+        cols = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', 
+            '#f58231', '#911eb4', '#46f0f0', '#f032e6', 
+            '#bcf60c', '#fabebe', '#008080', '#e6beff', 
+            '#9a6324', '#fffac8', '#800000', '#aaffc3', 
+            '#808000', '#ffd8b1', '#000075', '#808080', 
+            '#ffffff', '#000000']
+        fig = make_subplots(rows=1, cols=2,
+                            subplot_titles=("Cases", "Deaths", "Cases (Aligned to first case)", "Deaths (Aligned to first death)"))
+        keys = ['cases', 'deaths'] #, 'aligned_cases', 'aligned_deaths']
+        date1 = viz_map_utils.date_in_data(df)
+    #     print(date1)
+        date2 = [i for i in range(len(date1))]
+
+        # if multiple counties are selected, aka manual select
+        if isinstance(selected_counties, list):
+            for i in range(2):
+                key = keys[i]
+                for index, pos in enumerate(selected_counties):
+                    row = df[df['pos'] == pos]
+                    if i < 2:
+                        dates = date1
+                    else:
+                        dates = date2
+                    fig.add_trace(go.Scatter(x=dates,
+                                y=row[key].to_list()[0],
+                                line=dict(color=cols[index]),
+                                name = row['CountyName'].values[0] +', ' + row['StateName'].values[0],
+                                showlegend = i == 0
+                                ),row = i //2 + 1, col = i - (i// 2)*2 + 1) 
+        # if a single county is selected, aka nearest neighbors
+        elif selected_counties is not None:
+            # plot selected county
+            for i in range(2):
+                key = keys[i]
+                row = df[df['pos'] == selected_counties]
+                if i < 2:
+                    dates = date1
+                else:
+                    dates = date2
+                fig.add_trace(go.Scatter(x=dates,
+                            y=row[key].to_list()[0],
+                            line=dict(color=cols[0]),
+                            name = row['CountyName'].values[0] +', ' + row['StateName'].values[0],
+                            showlegend = i == 0
+                            ),row = i //2 + 1, col = i - (i// 2)*2 + 1)
+            # if features are selected, run nearest neighbors and plot results    
+            if selected_features is not None:
+                if len(selected_features)!= 0:
+                    neighs = nn.model(df, selected_features, selected_counties)
+                    # print(neighs)
+                    for i in range(2):
+                        key = keys[i]
+                        for index, pos in enumerate(neighs):
+                            row = df[df['pos'] == pos]
+                            if i < 2:
+                                dates = date1
+                            else:
+                                dates = date2
+                            # print(row['CountyName'].values[0] +', ' + row['StateName'].values[0])
+                            fig.add_trace(go.Scatter(x=dates,
+                                        y=row[key].to_list()[0],
+                                        line=dict(color=cols[index+1]),
+                                        name = row['CountyName'].values[0] +', ' + row['StateName'].values[0],
+                                        showlegend = i == 0
+                                        ),row = i //2 + 1, col = i - (i// 2)*2 + 1)
+
+        fig.update_layout(height=450,  
+                          width=900,
+                          template='plotly_dark',
+                          xaxis_title="Time",
+                          yaxis_title="Count",
+                          yaxis_showgrid=True)
+
+        # edit axis labels
+        fig['layout']['xaxis']['title']='Date'
+        fig['layout']['xaxis2']['title']='Date'
+#         fig['layout']['xaxis3']['title']='Days'
+#         fig['layout']['xaxis4']['title']='Days'
+        fig['layout']['yaxis']['title']='Cases'
+        fig['layout']['yaxis2']['title']='Deaths'
+#         fig['layout']['yaxis3']['title']='Cases'
+#         fig['layout']['yaxis4']['title']='Deaths'                                
+        fig.write_image('./matching.svg')
+        fig.write_image(oj(assets_dir, 'matching.svg'))
+    save_screenshot()
 
     @app.callback(
         dash.dependencies.Output('toggle-switch-output', 'children'),
@@ -309,6 +398,7 @@ if __name__ == '__main__':
         app = dash.Dash(__name__)
         app.config.suppress_callback_exceptions = True
         redefine_app(app, df)
+        add_callbacks(app, df)
         print('running matching app...last reloaded at', time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
         return app
 
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 path_to_main_dir = "/home/ubuntu/new_uploader/"
 data_dir = path_to_main_dir + "data"
 viz_dir = path_to_main_dir + "viz" 
+assets_dir = path_to_main_dir + 'assets'
